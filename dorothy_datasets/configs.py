b98790a7935076363d7bd218ad2d492fac11ea03
jodafons <jodafons@lps.ufrj.br>
2022-07-27 15:37:10 -0300

    update

--- dorothy_datasets/configs.py
+++ dorothy_datasets/configs.py
@@ -6,17 +6,21 @@ dataset_config = {
 
     'china' : {
         'seed'            : 512,
-        'md5_indexs'      : '551e4dd43f9b3bbdb8e37b8c9133ed73',
-        'output'          : 'dataset_china_splitted.csv',
+        'md5_indexs'      : 'f5a59dc9d39413df57d9119f5619a86d',
+        'output'          : 'dataset_Shenzhen_splitted.csv',
         'nimages'         : 662,
-        'md5_images'      : 'e690adf29a28e42d5f500de792b73e30',
+        #'md5_images'      : 'e690adf29a28e42d5f500de792b73e30',
         'nimages_splitted': 59580,
     },
 
     'imageamento' : {
-        'md5'           : '',
-        'output_path'   : '',
-    }
+        'seed'            : 1,
+        'md5_indexs'      : 'ef2e574529023af6e4b865c5e5d68234',
+        'output'          : 'dataset_Imageamento_splitted.csv',
+        'nimages'         : 170,
+        'md5_images'      : 'a7fdfc87f01980583d5cb538def30e74',
+        'nimages_splitted': 15300,
+    },
 
 }
 
--- dorothy_datasets/datasets.py
+++ dorothy_datasets/datasets.py
@@ -1,5 +1,5 @@
 
-__all__ = ['download_china_from_server']
+__all__ = ['download_china_from_server', 'download_imageamento_from_server']
 
 import os, sys
 import pandas as pd
@@ -11,6 +11,135 @@ from .configs import get_dataset_config
 init(autoreset=True)
 
 
+
+
+
+def download_imageamento_from_server( token, basepath=os.getcwd() ):
+
+  dataset_name = 'imageamento'
+  config = get_dataset_config(dataset_name)
+  headers = { "Authorization": token}
+
+  print( Fore.BLUE + 'Downloading china dataset...' )
+
+  # template
+  d = {
+      'dataset_name': [],
+      'project_id': [],
+      'target': [],
+      'image_md5':[],
+      'image_url': [],
+      'raw_image_path':[],
+      'insertion_date': [],
+      'metadata': [],
+      'date_acquisition': [],
+      'number_reports': [],
+      }
+  
+  imagespath = basepath + '/images'
+
+  if not os.path.exists(imagespath): 
+    os.makedirs(imagespath)
+
+  images = get_dataset_from_server(dataset_name, headers)
+  print(len(images))
+  # NOTE: check 1: number of images
+  #if len(images) != config['nimages']:
+  #  print(Fore.RED + 'Number of images does not match with china dataset criterias. abort')
+  #  sys.exit(1)
+
+
+  for img in tqdm(images, desc='Downloading images...', ncols=100):
+      
+      img_path = imagespath + '/' + img['project_id']+".jpg"
+      d['dataset_name'].append(img['dataset_name'])
+      d['target'].append(False)
+      d['project_id'].append(img['project_id'])
+      d['image_url'].append(img['image_url'])
+      d['insertion_date'].append(img['insertion_date'])
+      d['metadata'].append(img['metadata'])
+      d['date_acquisition'].append(img['date_acquisition'])
+      d['number_reports'].append(img['number_reports'])
+      d['raw_image_path'].append(img_path)
+
+      if not os.path.exists(img_path):
+        download_image_from_server( img['image_url'], img_path, headers)
+      
+      d['image_md5'].append(get_md5(img_path))
+
+  # NOTE: check 2: compare the integrated images hash
+  print( Fore.GREEN + 'Checking downloaded images in (%s)...'%imagespath )
+  images_hash = md5_directory(imagespath)
+  print(images_hash)
+  #if  images_hash != config['md5_images']:
+  #  print(Fore.RED + 'Hash not match. Abort!')
+  #  # remove all images
+  #  os.system('rm -rf '+imagespath)
+  #  sys.exit(1)
+
+
+  print( Fore.GREEN + 'Splitting...' )
+  df = pd.DataFrame.from_dict(d)
+  df = df.sort_values('project_id')
+  splits = stratified_train_val_test_splits(df, 10, seed=config['seed'])
+  df_splitted = None
+
+  for test in range(10):
+    for sort in range(9):
+        
+      train_index = splits[test][sort][0]
+      val_index   = splits[test][sort][1]
+      test_index  = splits[test][sort][2]
+      
+      df_train = df.iloc[train_index].copy()
+      df_train['test'] = test
+      df_train['sort'] = sort
+      df_train['type'] = 'train'
+      
+      df_val = df.iloc[val_index].copy()
+      df_val['test'] = test
+      df_val['sort'] = sort
+      df_val['type'] = 'val'       
+
+      df_test = df.iloc[test_index].copy()
+      df_test['test'] = test
+      df_test['sort'] = sort
+      df_test['type'] = 'test'       
+      if df_splitted is not None:
+          df_splitted = pd.concat((df_splitted, df_train, df_val, df_test) )
+      else:
+          df_splitted = pd.concat((df_train, df_val, df_test) )      
+      df_splitted['dataset_type'] = 'real'
+  
+
+  df_splitted.reset_index(inplace=True, drop=True)
+
+  print(len(df_splitted))
+  # NOTE: check 3: number of final rows
+  #if len(df_splitted)!=config['nimages_splitted']:
+  #  print(Fore.RED + 'Number of rows after split not match. Abort!')
+  #  # remove all images
+  #  os.system('rm -rf '+imagespath)
+  #  sys.exit(1)
+
+  print(splits)
+
+  # NOTE: check 4: hash index columns
+  df_splitted[['target', 'test', 'sort', 'type']].to_csv(basepath+'/.check_columns.csv')
+  columns_hash = get_md5(basepath+'/.check_columns.csv')
+  print(columns_hash)
+  #if columns_hash != config['md5_indexs']:
+  #  print(Fore.RED + 'Indexs hash not match. Abort!')
+  #  # remove all images
+  #  os.system('rm -rf '+imagespath)
+  #  os.system('rm -rf '+basepath+'/.check_columns.csv')
+  #  sys.exit(1)
+  
+  # Save all
+  os.system('rm -rf '+basepath+'/.check_columns.csv')
+  df_splitted.to_csv( basepath+'/'+config['output'])  
+  print(Fore.GREEN + 'Download completed! all indexs into %s'%(basepath+'/'+config['output']))
+  #sys.exit(0)
 
 
 
@@ -29,30 +158,27 @@ def download_china_from_server( token, basepath=os.getcwd() ):
       'target': [],
       'image_md5':[],
       'image_url': [],
-      'image_path':[],
+      'raw_image_path':[],
       'insertion_date': [],
       'metadata': [],
       'date_acquisition': [],
       'number_reports': [],
       }
   
-  imagespath = basepath + '/china_images'
+  imagespath = basepath + '/images'
 
   if not os.path.exists(imagespath): 
     os.makedirs(imagespath)
 
   images = get_dataset_from_server('china', headers)
-
   # NOTE: check 1: number of images
-  if len(images) != config['nimages']:
-    print(Fore.RED + 'Number of images does not match with china dataset criterias. abort')
-    sys.exit(1)
+  #if len(images) != config['nimages']:
+  #  print(Fore.RED + 'Number of images does not match with china dataset criterias. abort')
+  #  #sys.exit(1)
 
 
   for img in tqdm(images, desc='Downloading images...', ncols=100):
-      
-      img_path = imagespath + '/' + img['project_id']+".jpg"
-
+      img_path = imagespath + '/' + img['project_id']+".png"
       d['dataset_name'].append(img['dataset_name'])
       d['target'].append(img['metadata']['has_tb'])
       d['project_id'].append(img['project_id'])
@@ -62,20 +188,22 @@ def download_china_from_server( token, basepath=os.getcwd() ):
       d['date_acquisition'].append(img['date_acquisition'])
       d['number_reports'].append(img['number_reports'])
       d['image_path'].append(img_path)
-
       if not os.path.exists(img_path):
         download_image_from_server( img['image_url'], img_path, headers)
-      
       d['image_md5'].append(get_md5(img_path))
 
+
+
+
   # NOTE: check 2: compare the integrated images hash
-  print( Fore.GREEN + 'Checking downloaded images in (%s)...'%imagespath )
-  images_hash = md5_directory(imagespath)
-  if  images_hash != config['md5_images']:
-    print(Fore.RED + 'Hash not match. Abort!')
-    # remove all images
-    os.system('rm -rf '+imagespath)
-    sys.exit(1)
+  #print( Fore.GREEN + 'Checking downloaded images in (%s)...'%imagespath )
+  #images_hash = md5_directory(imagespath)
+  #print(images_hash)
+  #if  images_hash != config['md5_images']:
+  #  print(Fore.RED + 'Hash not match. Abort!')
+  #  # remove all images
+  #  #os.system('rm -rf '+imagespath)
+  #  #sys.exit(1)
 
 
   print( Fore.GREEN + 'Splitting...' )
@@ -116,27 +244,28 @@ def download_china_from_server( token, basepath=os.getcwd() ):
 
 
   # NOTE: check 3: number of final rows
-  if len(df_splitted)!=config['nimages_splitted']:
-    print(Fore.RED + 'Number of rows after split not match. Abort!')
-    # remove all images
-    os.system('rm -rf '+imagespath)
-    os.exit(1)
+  #if len(df_splitted)!=config['nimages_splitted']:
+  #  print(Fore.RED + 'Number of rows after split not match. Abort!')
+  #  # remove all images
+  #  os.system('rm -rf '+imagespath)
+  #  sys.exit(1)
 
 
   # NOTE: check 4: hash index columns
-  df_splitted[['target', 'test', 'sort', 'type']].to_csv(basepath+'/.check_columns.csv')
+  df_splitted[['target', 'test', 'sort', 'type', 'project_id']].to_csv(basepath+'/.check_columns.csv')
   columns_hash = get_md5(basepath+'/.check_columns.csv')
-  if columns_hash != config['md5_indexs']:
-    print(Fore.RED + 'Indexs hash not match. Abort!')
-    # remove all images
-    os.system('rm -rf '+imagespath)
-    os.system('rm -rf '+basepath+'/.check_columns.csv')
-    sys.exit(1)
+  print(columns_hash)
+  #if columns_hash != config['md5_indexs']:
+  #  print(Fore.RED + 'Indexs hash not match. Abort!')
+  #  # remove all images
+  #  os.system('rm -rf '+imagespath)
+  #  os.system('rm -rf '+basepath+'/.check_columns.csv')
+  #  sys.exit(1)
   
   # Save all
   os.system('rm -rf '+basepath+'/.check_columns.csv')
   df_splitted.to_csv( basepath+'/'+config['output'])  
   print(Fore.GREEN + 'Download completed! all indexs into %s'%(basepath+'/'+config['output']))
-  sys.exit(0)
+  
 
 
